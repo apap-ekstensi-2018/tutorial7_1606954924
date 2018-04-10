a331d81e43bc2c87fe0c8823c2a0a65a4f43463f
Novalina NVH. Hutabarat <novalina.hutabarat@moonlay.jakarta>
2018-04-10 21:37:40 +0700

    Tutorial07 Producer

---
+++ Tutorial07Consumer/src/main/java/com/example/dao/StudentDAOImpl.java
@@ -0,0 +1,26 @@
+package com.example.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.model.StudentModel;
+
+public class StudentDAOImpl implements StudentDAO {
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Override
+	public StudentModel selectStudent(String npm)
+	{
+		StudentModel student = restTemplate.getForObject("http://localhost:8080/rest/student/view/"+npm, StudentModel.class);
+		return student;
+	}
+	 
+	@Override
+	public List<StudentModel> selectAllStudents()
+	{
+		return null;
+	}
+}
